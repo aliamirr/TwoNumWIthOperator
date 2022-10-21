9666825301da2014e882c3e7fd7a267948f1d9da
Aamir <113689022+aliamirr@users.noreply.github.com>
2022-10-21 12:48:06 +0530

    Update Operator.java

--- src/Operator.java
+++ src/Operator.java
@@ -28,13 +28,15 @@ public class Operator{
         else if(op == '*'){
             ans = a * b;
             //modulo
-        }else if(op == '%'){
-            ans = a % b;
-            //division
-        } else if (op == '/') {
+        } else if (op == '/'){
             if(b != 0){
                 ans = a/b;
             }
+        }
+        else if(op == '%'){
+            ans = a % b;
+            //division
+        }
         
         //Print the final result
         System.out.println("Your answer is: " + ans);
@@ -42,3 +44,54 @@ public class Operator{
     }
 
 }
+
+//Anoter method
+
+//import java.util.Scanner;
+//
+//public class Calculator {
+//    public static void main(String[] args) {
+//        Scanner in =  new Scanner(System.in);
+
+//        // input two numbers
+//        System.out.print("Enter two numbers: ");
+//        int num1 = in.nextInt();
+//        int num2 = in.nextInt();
+
+//        // Take input from user till user does not press X or x.
+//        
+//        int ans = 0;
+//        while ( true ) {
+//            // take the operator as input
+//            System.out.print("Enter the operator: ");
+//            char op = in.next().trim().charAt(0);
+//
+//            if (op == '+' || op == '-' || op == '*' || op == '/' || op == '%') {
+//
+//                if (op == '+') {
+//                    ans = num1 + num2;
+//                }
+//                if (op == '-') {
+//                    ans = num1 - num2;
+//                }
+//                if (op == '*') {
+//                    ans = num1 * num2;
+//                }
+//                if (op == '/') {
+//                    if (num2 != 0) {
+//                        ans = num1 / num2;
+//                    }
+//                }
+//                if (op == '%') {
+//                    ans = num1 % num2;
+//                }
+//            }   else if (op == 'x' || op == 'X') {
+//                break;
+//                } else {
+//                    System.out.println("Invalid operation!!");
+//
+//                }
+//                System.out.println(ans);
+//        }
+//    }
+//}
